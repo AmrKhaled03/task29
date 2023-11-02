712697bde4338bb33eadcbd3a49a4166edeeb05d
AmrKhaled03 <kamr4014@gmail.com>
2023-11-02 14:51:59 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector(".pepsi").src=anything;
+}
+function changeColor(color){
+    const sec =document.querySelector(".content");
+sec.style.backgroundColor=color
+    document.querySelector('.navbar').style.backgroundColor = color
+}
